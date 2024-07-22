75ad0ef496fb4f2a05fd7f5b8753482c34033a4b
unknown <nssteenbeek@gmail.com>
2024-07-22 18:49:20 +0200

    finished part08_01_Cubes

---
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Cubes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                break;
+            }
+
+            int integer = Integer.valueOf(input);
+
+            int cube = integer * integer * integer;
+            System.out.println(cube);
+        }
+
+    }
+}
