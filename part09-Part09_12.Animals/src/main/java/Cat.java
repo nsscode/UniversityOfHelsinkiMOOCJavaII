1f7f22542c4f9f4f097e8567cc885e62b2820ee5
unknown <nssteenbeek@gmail.com>
2024-09-04 15:07:46 +0200

    finished part09_12_Animals

---
+++ part09-Part09_12.Animals/src/main/java/Cat.java
@@ -0,0 +1,18 @@
+public class Cat extends Animal implements NoiseCapable {
+    
+    public Cat(String name) {
+        super(name);
+    }
+
+    public Cat() {
+        super("Cat");
+    }
+
+    @Override
+    public void makeNoise() {
+        this.purr();
+    }
+    public void purr() {
+        System.out.println(this.getName() + " purrs");
+    }
+}
--- part09-Part09_12.Animals/src/main/java/Main.java
+++ part09-Part09_12.Animals/src/main/java/Main.java
@@ -4,6 +4,13 @@ public class Main {
     public static void main(String[] args) {
         // you can test how your classes work here
 
+        Cat cat = new Cat();
+        cat.purr();
+        cat.eat();
+        
+        Cat garfield = new Cat("Garfield");
+        garfield.purr();
+
     }
 
 }
